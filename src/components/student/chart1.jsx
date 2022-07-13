9c1d58a99ed1390aa3c3aea5c8396a47eed282ef
laherasif <laherasif@gmail.com>
2022-07-13 13:58:06 +0500

    setting dashboard

--- src/components/student/chart1.jsx
+++ src/components/student/chart1.jsx
@@ -7,23 +7,24 @@ import {
   Tooltip,
   Legend,
   ReferenceLine,
+  XAxis,
   ResponsiveContainer,
 } from "recharts";
-import {FaCircle} from 'react-icons/fa'
+import { FaCircle } from 'react-icons/fa'
 
 
-const Chart = ({ color, strokeColor, label, value , chart }) => {
+const Chart = ({ color, strokeColor, label, value, chart }) => {
   const CustomizedDot = (props) => {
     const { cx, cy, stroke, payload, value } = props;
 
     if (value == 18 || value == 12) {
       return (
         <svg x={cx - 10} y={cy - 10} >
- 
-          <FaCircle  size={12} fill={color} strokeWidth={60} stroke={strokeColor} >
 
-            </FaCircle>
-        
+          <FaCircle size={12} fill={color} strokeWidth={60} stroke={strokeColor} >
+
+          </FaCircle>
+
         </svg>
       );
     }
@@ -37,7 +38,7 @@ const Chart = ({ color, strokeColor, label, value , chart }) => {
         <p>{label}</p>
         <h5 style={{ color }}>{value}</h5>
       </div>
-      <div style={{ position: "relative", top: 20,right:80 }}>
+      <div style={{ position: "relative", top: 20, right: 80 }}>
         <ResponsiveContainer width={180} height={100}>
           <LineChart
             height={321}
@@ -58,6 +59,8 @@ const Chart = ({ color, strokeColor, label, value , chart }) => {
               dot={<CustomizedDot />}
               strokeWidth={3}
             />
+            <XAxis fontSize={16} tickMargin={16}
+              tickLine={false} axisLine={false} dataKey="name" style={{ display: 'none' }} />
             {/* <ReferenceLine y={10} x={10} label={<ReferenceLabel value="14222xx" />} strokeWidth={0} /> */}
           </LineChart>
         </ResponsiveContainer>
